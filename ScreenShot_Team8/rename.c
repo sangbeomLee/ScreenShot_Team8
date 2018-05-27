40850fdbe6ccb7abc190a9ff11bfcde8b09ef63a
yura jung <jungyr24@hanmail.net>
2018-05-27 16:25:34 +0900

    main문 병합 및 Rename함수 인자 변경

--- ScreenShot_Team8/rename.c
+++ ScreenShot_Team8/rename.c
@@ -1,16 +1,12 @@
 #include <Windows.h>
-<<<<<<< HEAD
 #include<stdio.h>
 #include<io.h>
 #include<stdlib.h>
 #include<malloc.h>
 #include "filename.h"
 typedef struct _finddata_t FILE_SEARCH;
-=======
-#include <string.h>
->>>>>>> origin/master
 
-char * Rename(char *FileName) {			// 파일이름 변경
+char * Rename(char *fName) {			// 파일이름 변경
 	int set = 0;
 	char *ptr;
 	FILE_SEARCH fd;
@@ -19,17 +15,15 @@ char * Rename(char *FileName) {			// 파일이름 변경
 	handle = _findfirst(".\\*.*", &fd);
 
 	printf("파일 이름 변경\n");
-	scanf("%s", FileName);
-	//TEXT("C:\\Users\\wing9\\Desktop\\ScreenShot.bmp");
-	//LPCTSTR refilename = TEXT("c:\ScreenShot.bmp");
+	scanf("%s", fName);
 	char CompareArr[100];
-	strcpy(CompareArr, FileName);
+	strcpy(CompareArr, fName);
 	strcat(CompareArr, ".bmp");
 	printf("comparearr :%s\n\n", CompareArr);
 	if (handle == -1)
 	{
 		printf("There were no files.\n");
-		return FileName;
+		return fName;
 	}
 
 	while (result != -1)
@@ -40,24 +34,20 @@ char * Rename(char *FileName) {			// 파일이름 변경
 		{
 			set = 1;
 			printf("동일한 이름이 있습니다. 기본 값으로 저장됩니다.\n");
-			FileName[0] = NULL;
-			ptr = SetFileName(FileName);
+			fName[0] = NULL;
+			ptr = SetFileName(fName);
 			break;
 		}
 		result = _findnext(handle, &fd);
 	}
 
-<<<<<<< HEAD
 	_findclose(handle);
 	if (set)
 		return ptr; // TODO : 밖에 FileName=filename();해줘야함
 	else
 	{
-		printf("변경된 파일명 : %s.bmp\n", FileName);
-		strcat(FileName, ".bmp");
-		return FileName;
+		printf("변경된 파일명 : %s.bmp\n", fName);
+		strcat(fName, ".bmp");
+		return fName;
 	}
 }
-=======
-}
->>>>>>> origin/master
--- ScreenShot_Team8/repath.c
+++ ScreenShot_Team8/repath.c
@@ -1,15 +1,8 @@
 #include<stdio.h>
 #include<string.h>
 
-<<<<<<< HEAD
-void rePath(LPCTSTR FileName) {			// 파일경로 변경
-	char path[100];
-
-	scanf("%s", &path);
-=======
 char * rePath() {			// 파일경로 변경
 	char temp[100];
->>>>>>> origin/master
 
 	printf("새로운 경로를 입력하세요 : ex).\\save\\\n");
 	scanf("%s", temp);
